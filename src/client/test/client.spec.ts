7e0f64e91a1c5de2d8c345e1d76151445de21284
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2017-08-03 21:08:31 +0200

    git subrepo fix

---
+++ src/client/test/client.spec.ts
@@ -0,0 +1,52 @@
+import { Client } from '..'
+import { EventEmitter } from "events"
+
+describe("Then deepstream client is up and running", () => {
+  class MockDeepstream extends EventEmitter {
+    constructor() {super()}
+    login = jasmine.createSpy()
+  }
+
+  let mockDeepstream
+  let DeepstreamStub
+  let loginData = {providerName: "UNIT TEST PROVIDER"}
+  let connectionString = "foobar"
+  
+  beforeEach(() => {
+    mockDeepstream = new MockDeepstream()
+
+    DeepstreamStub = jasmine.createSpy().and.callFake(() => {
+      return mockDeepstream
+    })
+  
+    spyOn(Client, 'GetDependencies').and.callFake(() => {
+      return {deepstream: DeepstreamStub}
+    })
+  })
+
+  it("should connect when the server is up", (done) => {
+    let client = new Client(connectionString)
+    expect(Client.GetDependencies).toHaveBeenCalled()
+    expect(DeepstreamStub).toHaveBeenCalledWith(connectionString)
+
+    let subscripion = client.login(loginData).subscribe(() => {
+      expect(mockDeepstream.login).toHaveBeenCalledWith(loginData)
+      done()
+    })
+
+    mockDeepstream.emit("connectionStateChanged", "OPEN")
+  })
+
+  it("should retry when some calls failed", (done) => {
+    let client = new Client(connectionString)
+
+    let subscripion = client.login(loginData).subscribe(() => {
+      done()
+    })
+
+    mockDeepstream.emit("connectionStateChanged", "ERROR")
+    mockDeepstream.emit("connectionStateChanged", "ERROR")
+    mockDeepstream.emit("error", "ERROR")
+    mockDeepstream.emit("connectionStateChanged", "OPEN")
+  })
+})
