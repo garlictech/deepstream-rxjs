b6af0757d4a0401426d36bb8b32285563524db25
Zsolt Molnar <zsolt@zsoltmolnar.hu>
2017-08-09 12:19:55 +0000

    feat: Logout support in deepstream-rxjs

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,27 +2,32 @@ import { Observable, Observer } from 'rxjs'
 import * as deepstream from 'deepstream.io-client-js'
 
 import { Record } from '../record'
-import { IProviderConnectionData} from '../interfaces'
+import { IProviderConnectionData, IConnectionData } from '../interfaces'
 
 export class Client {
-  private _record: Record;
-  public client: deepstreamIO.deepstreamQuarantine;
-  
-  public constructor(connectionString: string) {
-    this.client = Client.GetDependencies().deepstream(connectionString)
+  public client: deepstream.deepstreamQuarantine
+  private _record: Record
+
+  static GetDependencies() {
+    return {
+      deepstream: deepstream
+    }
   }
-  
+
+  public constructor(private _connectionString: string) {}
+
   public get record(): Record {
     if (!this._record) {
       this._record = new Record(this.client);
     }
-    
+
     return this._record;
   }
-  
-  public login(data: IProviderConnectionData): Observable < any > {
-    console.log(`Deepstream provider "${data.providerName || 'unknown'}" is logging in...`)
-    this.client.login(data)
+
+  public login(authData: IProviderConnectionData | IConnectionData): Observable < any > {
+    this.client = Client.GetDependencies().deepstream(this._connectionString)
+    console.log("Deepstream client is logging in... with data: ", JSON.stringify(authData, null, 2))
+    this.client.login(authData)
 
     let errObs = Observable.fromEvent(this.client, "error")
     .do(state => {
@@ -33,12 +38,21 @@ export class Client {
     return Observable.fromEvent(this.client, "connectionStateChanged")
     .do(state => console.log("Deepstream client connection state: ", state))
     .filter(state => state === 'OPEN')
+    .take(1)
     .do(() => errObs.unsubscribe())
   }
 
-  static GetDependencies() {
-    return {
-      deepstream: deepstream
-    }
+  public logout() {
+    let obs$ = Observable.fromEvent(this.client, "connectionStateChanged")
+    .do(state => console.log("Closing client connection state: ", state))
+    .filter(state => state === 'CLOSED')
+    .take(1)
+    .do(() => console.log(`Deepstream client is logged out`))
+    this.client.close()
+    return obs$
+  }
+
+  public isConnected(): boolean {
+    return this.client && this.client.getConnectionState() === deepstream.CONSTANTS.CONNECTION_STATE.OPEN
   }
 }
