3cba7dd0875e318e381c259b028d9ec9fc45a47e
Zsolt R. Molnar <zsolt@zsoltmolnar.hu>
2017-08-25 19:04:50 +0200

    Merge pull request #6 from tujlaky/master
    deepstream-rxjs: Refactor record and list to standalone classes

--- src/client/test/client.spec.ts
+++ src/client/test/client.spec.ts
@@ -1,64 +1,71 @@
 import { Client } from '..'
 import { Record } from '../../record';
 
-import { EventEmitter } from "events"
+import { EventEmitter } from 'events'
 let deepstream = require('deepstream.io-client-js')
 
-describe("When the deepstream client is up and running, the client", () => {
+describe('When the deepstream client is up and running, the client', () => {
   class MockDeepstream extends EventEmitter {
     private _state = deepstream.CONSTANTS.CONNECTION_STATE.OPEN
-    constructor() {super()}
-    login = jasmine.createSpy("login").and.callFake(() => {
+    constructor() {
+      super()
+    }
+
+    public login = jasmine.createSpy('login').and.callFake(() => {
       this._state = deepstream.CONSTANTS.CONNECTION_STATE.OPEN
     })
-    close = jasmine.createSpy("close").and.callFake(() => {
+
+    public close = jasmine.createSpy('close').and.callFake(() => {
       this._state = deepstream.CONSTANTS.CONNECTION_STATE.CLOSED
     })
-    getConnectionState() { return this._state }
+
+    public getConnectionState() {
+      return this._state
+    }
   }
 
   let mockDeepstream: any
-  let DeepstreamStub
-  let loginData = {providerName: "UNIT TEST PROVIDER", jwtToken: "foobar"}
-  let connectionString = "foobar"
-  
+  let deepstreamStub
+  let loginData = {providerName: 'UNIT TEST PROVIDER', jwtToken: 'foobar'}
+  let connectionString = 'foobar'
+
   beforeEach(() => {
     mockDeepstream = new MockDeepstream()
 
-    DeepstreamStub = jasmine.createSpy("DeepstreamStub").and.callFake(() => {
+    deepstreamStub = jasmine.createSpy('deepstreamStub').and.callFake(() => {
       return mockDeepstream
     })
-  
+
     spyOn(Client, 'GetDependencies').and.callFake(() => {
-      return {deepstream: DeepstreamStub}
+      return {deepstream: deepstreamStub}
     })
   })
-  
-  it("should connect", (done) => {
+
+  it('should connect', (done) => {
     let client = new Client(connectionString)
     expect(client.isConnected()).toBeFalsy()
 
     client.login(loginData).subscribe(() => {
-      expect(DeepstreamStub).toHaveBeenCalledWith(connectionString)
+      expect(deepstreamStub).toHaveBeenCalledWith(connectionString)
       expect(Client.GetDependencies).toHaveBeenCalled()
       expect(mockDeepstream.login).toHaveBeenCalledWith(loginData)
       expect(client.isConnected()).toBeTruthy()
       done()
     })
 
-    mockDeepstream.emit("connectionStateChanged", "OPEN")
+    mockDeepstream.emit('connectionStateChanged', 'OPEN')
   })
 
-  it("should retry when some calls failed", (done) => {
+  it('should retry when some calls failed', (done) => {
     let client = new Client(connectionString)
     let subscripion = client.login(loginData).subscribe(() => done())
-    mockDeepstream.emit("connectionStateChanged", "ERROR")
-    mockDeepstream.emit("connectionStateChanged", "ERROR")
-    mockDeepstream.emit("error", "ERROR")
-    mockDeepstream.emit("connectionStateChanged", "OPEN")
+    mockDeepstream.emit('connectionStateChanged', 'ERROR')
+    mockDeepstream.emit('connectionStateChanged', 'ERROR')
+    mockDeepstream.emit('error', 'ERROR')
+    mockDeepstream.emit('connectionStateChanged', 'OPEN')
   })
 
-  it("should be able to logout", (done) => {
+  it('should be able to logout', (done) => {
     let client = new Client(connectionString)
     client.login(loginData)
     .do(() => expect(client.isConnected()).toBeTruthy())
@@ -72,23 +79,23 @@ describe("When the deepstream client is up and running, the client", () => {
       expect(client.isConnected()).toBeFalsy()
       done()
     })
-    
-    mockDeepstream.emit("connectionStateChanged", "OPEN")
+
+    mockDeepstream.emit('connectionStateChanged', 'OPEN')
     // This will test if the logout observer emits only at the 'close' event
-    mockDeepstream.emit("connectionStateChanged", "FOOBAR")
-    mockDeepstream.emit("connectionStateChanged", "CLOSED")
+    mockDeepstream.emit('connectionStateChanged', 'FOOBAR')
+    mockDeepstream.emit('connectionStateChanged', 'CLOSED')
   })
 
-  it("should handle logout request even if the client is logged out", (done) => {
+  it('should handle logout request even if the client is logged out', (done) => {
     let client = new Client(connectionString)
     client.logout()
-    .subscribe(() =>{
+    .subscribe(() => {
       expect(mockDeepstream.close).not.toHaveBeenCalled()
       done()
     })
   })
 
-  it("should handle re-login: if the client is logged in, close the connection", (done) => {
+  it('should handle re-login: if the client is logged in, close the connection', (done) => {
     let client = new Client(connectionString)
     client.login(loginData)
     .do(() => {
@@ -98,27 +105,17 @@ describe("When the deepstream client is up and running, the client", () => {
       let res$ = client.login(loginData)
       return res$
     })
-    .subscribe(() =>{
+    .subscribe(() => {
       expect(mockDeepstream.close).toHaveBeenCalled()
       done()
     })
-    mockDeepstream.emit("connectionStateChanged", "OPEN")
-    mockDeepstream.emit("connectionStateChanged", "OPEN")
+    mockDeepstream.emit('connectionStateChanged', 'OPEN')
+    mockDeepstream.emit('connectionStateChanged', 'OPEN')
   })
-
-  it('should get the record instance', (done) => {
-    let client = new Client(connectionString)
-    
-    expect(client.record).not.toBeUndefined();
-    expect(client.record instanceof Record).toBeTruthy();
-
-    done();
-  });
 })
 
-describe("Without mocking the deepstream dependencies", () => {
+describe('Without mocking the deepstream dependencies', () => {
   it('GetDependencies should return the deepstream factory', () => {
     expect(Client.GetDependencies()).not.toBeNull
   });
 })
-
---
+++ src/list/test/list.spec.ts
@@ -0,0 +1,130 @@
+import { Observable } from 'rxjs';
+
+import { List } from '..'
+import { Client } from '../../client'
+
+describe('Test List', () => {
+  let addEntrySpy: any;
+  let removeEntrySpy: any;
+  let getListSpy: any;
+  let listName = 'listName';
+
+  let data  = ['1', '2'];
+  let data2 = ['1', '2', '3'];
+
+  class MockClient extends Client {
+    public client = {
+      record: {
+        getList: getListSpy
+      }
+    }
+  }
+
+  describe('When we try to get the data', () => {
+    it('should return an observable', async () => {
+      getListSpy = jasmine.createSpy('getList').and.callFake((name) => {
+        return {
+          whenReady: (callback => callback()),
+          subscribe: (callback) => {
+            callback(data);
+          },
+          unsubscribe: () => {}
+        };
+      });
+
+      let client = new MockClient('atyala');
+      let list = new List(client, listName);
+
+      let list$ = list.get();
+      expect(list$ instanceof Observable).toBeTruthy();
+
+      let result = await list$.take(1).toPromise();
+      let args = getListSpy.calls.mostRecent().args;
+
+      expect(args[0]).toEqual(listName);
+      expect(result instanceof Array).toBeTruthy();
+      expect(result).toEqual(data);
+    });
+
+    it('should call observer\'s next when data changed', (done) => {
+      getListSpy = jasmine.createSpy('getList').and.callFake((name) => {
+        return {
+          whenReady: (callback => callback()),
+          subscribe: (callback) => {
+            callback(data);
+
+            setTimeout(() => {
+              callback(data2)
+            }, 100);
+          },
+          unsubscribe: () => {}
+        };
+      });
+
+      let client = new MockClient('atyala');
+      let list = new List(client, listName);
+
+      let list$ = list.get();
+      expect(list$ instanceof Observable).toBeTruthy();
+
+      list$
+        .skip(1)
+        .subscribe(list => {
+          expect(list instanceof Array).toBeTruthy();
+          expect(list).toEqual(data2);
+          done();
+        }, done.fail)
+    });
+  })
+
+  describe('When we try to add any data', () => {
+    it('should do it', () => {
+      addEntrySpy = jasmine.createSpy('addEntry');
+
+      getListSpy = jasmine.createSpy('getList').and.callFake((name) => {
+        return {
+          whenReady: (callback => callback()),
+          subscribe: (callback) => {
+            callback(data);
+          },
+          addEntry: addEntrySpy,
+          unsubscribe: () => {}
+        };
+      });      
+
+      let client = new MockClient('atyala');
+      let list = new List(client, listName);
+
+      list.addEntry('5');
+
+      let args = addEntrySpy.calls.mostRecent().args;
+      expect(args[0]).toEqual('5');
+    });
+  });
+
+  describe('When we try to remove any data', () => {
+    it('should do it', () => {
+      removeEntrySpy = jasmine.createSpy('removeEntry');
+
+      getListSpy = jasmine.createSpy('getList').and.callFake((name) => {
+        return {
+          whenReady: (callback => callback()),
+          subscribe: (callback) => {
+            callback(data);
+          },
+          removeEntry: removeEntrySpy,
+          unsubscribe: () => {}
+        };
+      });      
+
+      let client = new MockClient('atyala');
+      let list = new List(client, listName);
+
+      list.removeEntry('5');
+
+      let args = removeEntrySpy.calls.mostRecent().args;
+      expect(args[0]).toEqual('5');
+    })
+  })
+
+})
