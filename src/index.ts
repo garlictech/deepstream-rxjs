2df417109bc72eba276d8061e17daba44a1c5d45
Gergely Koncz <gergo.koncz@gmail.com>
2018-05-29 12:25:39 +0200

    feat: Export Event

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export { Record } from './record';
 export { List } from './list';
 export { Query } from './query';
 export { Rpc } from './rpc';
+export { Event } from './event';
 export * from './interfaces';
